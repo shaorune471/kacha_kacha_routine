a0cee774a0dc62e777599dd16eb65769760933a5
mizuki <raikou2471@gmail.com>
2026-05-10 12:51:51 +0900

    デバッグログの削除

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,7 +12,6 @@ class ContactsController < ApplicationController
     if response.status_code.to_i.between?(200, 299)
       redirect_to contact_path, notice: "お問い合わせを送信しました。"
     else
-      Rails.logger.error "SendGrid error: #{response.status_code} #{response.body}"
       flash[:alert] = "送信に失敗しました。時間をおいて再度お試しください。"
       render :new, status: :unprocessable_entity
     end
