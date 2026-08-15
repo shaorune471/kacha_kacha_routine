4af0c10bf220bdc1ec6766935910aae2062a2be4
mizuki <raikou2471@gmail.com>
2026-08-15 23:38:20 +0900

    SendGridの無料プラン終了に伴いResendに移行

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,13 +85,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV["APP_HOST"], protocol: "https" }
 
   config.action_mailer.delivery_method = :resend
-  config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
-    authentication: :plain,
-    user_name: "apikey",
-    password: ENV["SENDGRID_API_KEY"]
-  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
